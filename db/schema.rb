c1442287d0231baa76da3da7c93b5c774522a4ec
Tatsuya Sato <satoryu.1981@gmail.com>
2020-03-16 01:23:22 +0900

    Use postgres in all environments

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_15_005336) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
