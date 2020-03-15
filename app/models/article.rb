fbb78842eb5cf7d6c610c734f5a40790f73356c4
Tatsuya Sato <satoryu.1981@gmail.com>
2020-03-15 19:15:45 +0900

    Create minimal models required for blog system

---
+++ app/models/article.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Article < ApplicationRecord
+  has_many :comments
+end
