2fd5c55133914751384b08b1436b6d4373dfe310
Tatsuya Sato <satoryu.1981@gmail.com>
2020-03-22 17:27:33 +0900

    Write a system test for creating new article

--- spec/system/posts_articles_spec.rb
+++ spec/system/posts_articles_spec.rb
@@ -6,10 +6,26 @@ RSpec.describe "PostsArticles", type: :system do
   end
 
   describe 'creating new article' do
-    it 'shows the form' do
+    before do
       visit new_article_path
+    end
 
+    it 'shows the form' do
       expect(page).to have_text 'New Article'
+    end
+
+    describe 'Then submitting article data' do
+      before do
+        fill_in 'Title', with: 'My First Article'
+        fill_in 'Body', with: 'Hello, Yabe'
+
+        click_button 'Create'
+      end
+
+      it 'shows posted new article' do
+        expect(page).to have_text 'My First Article'
+        expect(page).to have_text 'Hello, Yabe'
+      end
     end
   end
 end
