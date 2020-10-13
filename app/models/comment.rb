243b496ef9a69e0d83778107c7e5c6d6f92703a6
Tatsuya Sato <satoryu.1981@gmail.com>
2020-10-13 12:33:05 +0900

    Add annotations to the models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  visible    :boolean
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  article_id :integer          not null
+#
+# Indexes
+#
+#  index_comments_on_article_id  (article_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (article_id => articles.id)
+#
+
 class Comment < ApplicationRecord
   belongs_to :article
 end
--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  body       :text
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryBot.define do
   factory :article do
     title { Faker::Lorem.sentence }
