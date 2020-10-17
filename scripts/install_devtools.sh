12c8e737a0e214a73a880c2fd372454e670d3acb
Tatsuya Sato <satoryu.1981@gmail.com>
2020-10-17 12:40:54 +0900

    Enable doker extention from the container

--- scripts/install_devtools.sh
+++ scripts/install_devtools.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 apk update
+
+# Installs packages to comlile ext gems
 apk add --no-cache libxml2-dev curl-dev make gcc libc-dev g++
+
+# Installs dev tools
 apk add --no-cache git vim less
+
+# Installs Docker CLI
+apk add --no-cache docker-cli
